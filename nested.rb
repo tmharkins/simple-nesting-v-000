5e7928a6c5fbc7f66486d1e1075be346d8d23cc7
tmharkins <tmharkins@gmail.com>
2016-02-19 21:41:14 -0600

    solved simple nest hash lab

--- nested.rb
+++ nested.rb
@@ -1,5 +1,5 @@
-
-def hopper
+require 'pry'
+def hopper  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -79,8 +83,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
+    #This will add key/value pair to existing hash
+    programmer_hash = {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP, C"]}}
 
-    
 end
 
 def changing_alan
@@ -101,8 +106,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
+          
 end
 
 def adding_to_dennis
@@ -122,7 +128,8 @@ def adding_to_dennis
         }
      }
 
-     
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
 
 
